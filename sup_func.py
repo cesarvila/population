fb2050c300f169ed65568e91e3e2465c024d1b7e
César Vila Ambrona <cesarvila@gmail.com>
2020-04-28 18:47:37 +0200

    Refactoring: 3 coefs right
    Having realized that the free healthcare, mid age and % urb are the values most related with COVID-19 cases, I am getting insights to show that into my post

--- sup_func.py
+++ sup_func.py
@@ -1,4 +1,13 @@
 #sup_func
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+import seaborn as sns
+
+
 def popu_cifra(x):
     if x >= 1e9:
         s = '{:1.2f}B'.format(x*1e-9)
@@ -30,9 +39,9 @@ def edit_df(df, resp):
     if resp == 'TotalCases':
         df = df.drop(['TotalDeaths','TotalRecovered'],axis=1)
     elif resp == 'TotalDeaths':
-        df.drop(['TotalCases','TotalRecovered'],axis=1, inplace=True)
+        df = df.drop(['TotalCases','TotalRecovered'],axis=1)
     elif resp == 'TotalRecovered':
-        df.drop(['TotalCases','TotalDeaths'],axis=1, inplace=True)
+        df = df.drop(['TotalCases','TotalDeaths'],axis=1)
     else:
         print('error, no right input')
     df = df.dropna(subset=[resp], axis=0)
@@ -40,7 +49,7 @@ def edit_df(df, resp):
     df['TotalTests'].fillna(0, inplace=True)
     df['Med. Age'].fillna((df['Med. Age'].mean()), inplace=True)
     df['Urban Pop %'].fillna((df['Urban Pop %'].mean()), inplace=True)
-    y= df[resp]
+    y = df[resp]
     df = df.drop([resp], axis=1)
     X = df
 
@@ -59,10 +68,9 @@ def get_stat_data(X,y):
     rsquared_score = r2_score(y_test, y_test_preds)
     length_y_test = len(y_test)
     length_y_train = len(y_train)
-    test_score= r2_score(y_test, y_test_preds)
-    train_score = r2_score(y_train, y_train_preds)
-    return test_score, train_score, y_test, y_train
-
+    r2_scores_test= r2_score(y_test, y_test_preds)
+    r2_scores_train = r2_score(y_train, y_train_preds)
+    return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
 
 
 #Use the function to create X and y
@@ -128,3 +136,50 @@ def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=Tr
     lm_model.fit(X_train, y_train)
 
     return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
+
+def read_shapefile(sf):
+    #fetching the headings from the shape file
+    fields = [x[0] for x in sf.fields][1:]#fetching the records from the shape file
+    records = [list(i) for i in sf.records()]
+    shps = [s.points for s in sf.shapes()]#converting shapefile data into pandas dataframe
+    df = pd.DataFrame(columns=fields, data=records)#assigning the coordinates
+    df = df.assign(coords=shps)
+    return df
+
+def coef_weights(coefficients, X_train):
+    '''
+    INPUT:
+    coefficients - the coefficients of the linear model
+    X_train - the training data, so the column names can be used
+    OUTPUT:
+    coefs_df - a dataframe holding the coefficient, estimate, and abs(estimate)
+
+    Provides a dataframe that can be used to understand the most influential coefficients
+    in a linear model by providing the coefficient estimates along with the name of the
+    variable attached to the coefficient.
+    '''
+    coefs_df = pd.DataFrame()
+    coefs_df['est_int'] = X_train.columns
+    coefs_df['coefs'] = coefficients
+    coefs_df['abs_coefs'] = np.abs(coefficients)
+    coefs_df = coefs_df.sort_values('abs_coefs', ascending=False)
+    return coefs_df
+    
+def plot_shape(id, s=None):
+    plt.figure()
+    #plotting the graphical axes where map ploting will be done
+    ax = plt.axes()
+    ax.set_aspect('equal')#storing the id number to be worked upon
+    shape_ex = sf.shape(id)#NP.ZERO initializes an array of rows and column with 0 in place of each elements
+    #an array will be generated where number of rows will be(len(shape_ex,point))and number of columns will be 1 and stored into the variable
+    x_lon = np.zeros((len(shape_ex.points),1))#an array will be generated where number of rows will be(len(shape_ex,point))and number of columns will be 1 and stored into the variable
+    y_lat = np.zeros((len(shape_ex.points),1))
+    for ip in range(len(shape_ex.points)):
+        x_lon[ip] = shape_ex.points[ip][0]
+        y_lat[ip] = shape_ex.points[ip][1]#plotting using the derived coordinated stored in array created by numpy
+    plt.plot(x_lon,y_lat)
+    x0 = np.mean(x_lon)
+    y0 = np.mean(y_lat)
+    plt.text(x0, y0, s, fontsize=10)# use bbox (bounding box) to set plot limits
+    plt.xlim(shape_ex.bbox[0],shape_ex.bbox[2])
+    return x0, y0
